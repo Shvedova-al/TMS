f851b91623d18103c25d1a20ac44e7e3fb96fde4
Shvedova-al <Shvedova.a.e@gmail.com>
2022-01-30 16:19:17 +0300

    all tasks

--- Lesson3/src/main/java/Homework3.java
+++ Lesson3/src/main/java/Homework3.java
@@ -1,63 +1,149 @@
+
 import java.util.Scanner;
 
 public class Homework3 {
-
     public static void main(String[] args) {
 
         // Task1
         for (char alpha = 'a'; alpha <= 'z'; alpha++) {
-            System.out.println(alpha);
+            System.out.print(alpha);
         }
+        System.out.println("\n");
+        for (char alpha = 'A'; alpha <= 'Z'; alpha++) {
+            System.out.print(alpha);
+        }
+        System.out.print("\n");
 
         //Task2
-        {
-            System.out.println("Введите целое число");
 
-            Scanner in = new Scanner(System.in);
-            if (in.hasNextInt()) {
-                int num = in.nextInt();
-                if (num % 2 == 0) {
-                    System.out.println("Вы ввели четное число");
-                } else {
-                    System.out.println("Вы ввели нечетное число");
-                }
+        System.out.println("Введите целое число");
+
+        Scanner in = new Scanner(System.in);
+        if (in.hasNextInt()) {
+            int num = in.nextInt();
+            if (num % 2 == 0) {
+                System.out.println("Вы ввели четное число");
             } else {
-                System.out.println("Введенное число не соответствует условию");
+                System.out.println("Вы ввели нечетное число");
             }
+        } else {
+            System.out.println("Введенное число не соответствует условию");
         }
+
 
         //Task3
-        {
-            System.out.println("Введите 3 вещественных числа");
-            Scanner scan = new Scanner(System.in);
-            float num1 = scan.nextFloat();
-            float num2 = scan.nextFloat();
-            float num3 = scan.nextFloat();
 
-            num1 = Math.abs(num1);
-            num2 = Math.abs(num2);
-            num3 = Math.abs(num3);
+        System.out.println("Введите 3 вещественных числа");
+        Scanner scan = new Scanner(System.in);
+        double num1 = scan.nextDouble();
+        double num2 = scan.nextDouble();
+        double num3 = scan.nextDouble();
 
-            float min1 = Math.min(num1, num2);
-            float min2 = Math.min(min1, num3);
-            System.out.println("Наименьшее вещественное число по модулю:\n" + min2);
+        num1 = Math.abs(num1);
+        num2 = Math.abs(num2);
+        num3 = Math.abs(num3);
+
+        double min = Math.min(num1, num2);
+        System.out.println(Math.min(min, num3));
+
+
+        //Task4.1 correct
+
+        System.out.println("Введите число от 0 до 28800");
+
+        Scanner n = new Scanner(System.in);
+        int num = n.nextInt();
+        System.out.printf("%d\n", num);
+        for (
+                int hour = 7, sec = 25200;
+                hour >= 1; hour--, sec -= 3600) {
+            if (num < 28800 && num >= sec) {
+                System.out.printf("До конца рабочего дня %dч.\n", hour);
+                break;
+            } else if (num == 28800) {
+                System.out.println("До конца рабочего дня 8ч.");
+                break;
+            } else if (num < 3600) {
+                System.out.println("До конца рабочего дня менее часа");
+                break;
+            }
+
         }
 
 
-        //Task4
-        {
-            System.out.println("Введите число от 0 до 28800");
+        //Task 4.2
+/*
 
-            Scanner scan = new Scanner(System.in);
-            int num = scan.nextInt();
-            System.out.printf("%d\n", num);
-            for (
-                    int hour = 8, sec = 25200;
-                    hour >= 0; hour--, sec -= 3600) {
-                if (num <= 28800 && num > sec) {
-                    System.out.printf("Осталось %d часов\n", hour);
-                    break;
+            private static void display (int value) {
+                Random random = new Random();
+                value = random.nextInt(28800);
+                display(value);
+
+                int hour = value / 3600;
+                String result;
+                switch (hour) {
+                    case 0:
+                        result = "Осталось менее часа";
+                        break;
+                    case 1:
+                        result = "Остался 1 час";
+                        break;
+                    case 2:
+                        result = "Осталось 2 часа";
+                        break;
+                    case 3:
+                        result = "Осталось 3 часа";
+                        break;
+                    case 4:
+                        result = "Осталось 4 часа";
+                        break;
+                    case 5:
+                        result = "Осталось 5 часов";
+                        break;
+                    case 6:
+                        result = "Осталось 6 часов";
+                        break;
+                    case 7:
+                        result = "Осталось 7 часов";
+                        break;
+                    case 8:
+                        result = "Осталось 8 часов";
+                        break;
+
                 }
+                System.out.println(result);
+            }
+        }
+*/
+        //Task 5
+
+        System.out.println("       byte	 short  char   int   long  float  double   boolean");
+        System.out.println("byte:    т    ня     я     ня     ня     ня     ня        х   ");
+        System.out.println("short:   я     т     я     ня     ня     ня     ня        х   ");
+        System.out.println("char:    я     я     т     ня     ня     ня     ня        х   ");
+        System.out.println("int:     я     я     я      т     ня     ня     ня        х   ");
+        System.out.println("long:    я     я     я      я      т     ня     ня        х   ");
+        System.out.println("float:   я     я     я      я      я      т     ня        х   ");
+        System.out.println("double:  я     я     я      я      я      я      т        х   ");
+    }
+}
+/*
+        //Task 6
+        public static int average ( int[] array){
+            int sum = 0;
+            for (int i = 0; i < array.length; i++) {
+                sum = sum + array[i];
+            }
+            return (double) sum / array.length;
+
+
+            public static int max ( int[] array){
+                int maxvalue = 0;
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i] > maxvalue)
+                        maxvalue = array[i];
+                }
+                return max;
             }
         }
     }
@@ -65,16 +151,9 @@ public class Homework3 {
 
 
 
+
+
 /*
-
-
-
-
-
-
-
-
-
     //Задачи:
     //1) Используя тип данных char и операцию инкремента вывести на консоль все буквы английского алфавита
 
@@ -116,7 +195,7 @@ public class Homework3 {
 //    т  - если преобразование тождественно.
 //    Внимание! используйте System.out.printLn
 
-    /**
+
      * 6) Метод должен вернуть среднее значение из массива чисел
      * (необходимо сумму всех элеменов массива разделить на длину массива)
      * <p>
@@ -124,11 +203,10 @@ public class Homework3 {
      * array = {1,2,3,4,5}
      * Метод должен return 3.0
 
-    public static double average(int[] array) {
-        return 0;
+
     }
 
-    /**
+
      * 7) Метод должен вернуть максимальый элемент массива. Пример: array = {1,2,10,3} метод возвращает 10
 
     public static int max(int[] array) {
