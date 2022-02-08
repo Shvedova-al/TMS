c63b83f8572a603b86ac72d5e5c7a38a31eb6b28
Shvedova-al <Shvedova.a.e@gmail.com>
2022-02-08 10:28:32 +0300

    Task2

---
+++ Lesson7/src/main/java/Car.java
@@ -0,0 +1,72 @@
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Car {
+
+
+    private final String model;
+    private final String color;
+    static Engine engine = new Engine();
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        start();
+    }
+
+    private static void start() {
+        checkEngine();
+
+    }
+
+    private static void checkEngine() {
+
+        engine.start();
+    }
+
+
+    static void movement() {
+
+        int distance = 0;
+        String answer2 = null;
+        do {
+            System.out.println("Поехали? да/нет");
+            Scanner scanner = new Scanner(System.in);
+            String answer = scanner.next();
+            switch (answer) {
+                case "да":
+                    distance += 85;
+                    System.out.println("Вжж___ ___ ___ Приехали.\nЗакончить поездку? да/нет");
+                    Scanner scaner = new Scanner(System.in);
+                    answer2 = scaner.next();
+                    switch (answer2) {
+                        case "да":
+                            System.out.printf("На машине проехали всего %dкм.\n", distance);
+                            engine.stop();
+                        case "нет":
+                            continue;
+                    }
+                case "нет":
+                    System.out.printf("На машине проехали всего %dкм.\n", distance);
+                    engine.stop();
+            }
+        } while (Objects.equals(answer2, "нет"));
+
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
---
+++ Lesson7/src/main/java/Engine.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Engine {
+
+    public void start() {
+        System.out.println("Запуск двигателя");
+        Random random = new Random();
+        int start = random.nextInt(2);
+        System.out.println(start);
+        switch (start) {
+            case 0:
+                System.out.println("Двигатель запущен.");
+                Car.movement();
+            case 1:
+                System.out.println("Двигатель заглох");
+                stop();
+        }
+
+    }
+
+    public void stop() {
+        System.out.println("Хотите выключить машину? да/нет");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
+        switch (answer) {
+            case "нет" -> start();
+            case "да" -> System.out.println("Машина выключена");
+
+
+        }
+
+    }
+
+}
